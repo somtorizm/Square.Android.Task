e26ed1d0c7f51fc49b4dedd51cef1b6712e0db23
Ezinwa Victor <42660335+somtorizm@users.noreply.github.com>
2024-05-14 23:07:47 +0100

    Feature/plug in data (#3)
    * implement images plugin and caches
    
    * clean code
    
    * adjust ui
    
    * implement sorting
    
    * fix loading bug
    
    * add pull to refresh

--- app/src/main/java/com/vectorincng/squareandroidtaskvictor/data/EmployeesRepository.kt
+++ app/src/main/java/com/vectorincng/squareandroidtaskvictor/data/EmployeesRepository.kt
@@ -1,43 +1,36 @@
 package com.vectorincng.squareandroidtaskvictor.data
 
-import android.util.Log
 import com.vectorincng.squareandroidtaskvictor.network.EmployeeFetcher
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class EmployeesRepository @Inject constructor(
+interface EmployeesRepository {
+   suspend fun updateEmployeeList(force: Boolean) : Flow<EmployeeFetcher.EmployeeDataResponse>
+}
+
+class EmployeesRepositoryImpl @Inject constructor(
     private val employeeFetcher: EmployeeFetcher,
     @Dispatcher(SquareAppDispatcher.Main) mainDispatcher: CoroutineDispatcher
-) {
+): EmployeesRepository {
     private var refreshingJob: Job? = null
 
     private val scope = CoroutineScope(mainDispatcher)
-
-    suspend fun updatePodcasts(force: Boolean) {
+    override suspend fun updateEmployeeList(force: Boolean): Flow<EmployeeFetcher.EmployeeDataResponse> {
         if (refreshingJob?.isActive == true) {
             refreshingJob?.join()
         } else if (force) {
 
             refreshingJob = scope.launch {
                 // Now fetch the podcasts, and add each to each store
-                employeeFetcher().collect { state ->
-                    when(state){
-                        is EmployeeFetcher.EmployeeDataResponse.Error -> {
-                            Log.d("Hi","${state.throwable?.message}")
-
-                        }
-                        is EmployeeFetcher.EmployeeDataResponse.Success -> {
-                            Log.d("Hi",state.employees.size.toString())
-                        }
-                        null -> {
-
-                        }
-                    }
-                }
+                employeeFetcher.invoke()
             }
+
         }
+        return  employeeFetcher.invoke()
     }
+
 }
