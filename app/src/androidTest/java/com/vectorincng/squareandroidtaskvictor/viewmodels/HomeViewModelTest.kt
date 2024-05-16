6ef2bba6b39f1493dd8133f352e1d37bd1dd3607
victorsomto <ezinwavictor25@gmail.com>
2024-05-16 22:32:19 +0100

    add unit test

--- app/src/androidTest/java/com/vectorincng/squareandroidtaskvictor/viewmodels/HomeViewModelTest.kt
+++ app/src/androidTest/java/com/vectorincng/squareandroidtaskvictor/viewmodels/HomeViewModelTest.kt
@@ -1,17 +1,23 @@
 package com.vectorincng.squareandroidtaskvictor.viewmodels
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import app.cash.turbine.test
 import com.vectorincng.squareandroidtaskvictor.MainCoroutineRule
 import com.vectorincng.squareandroidtaskvictor.data.EmployeesRepository
+import com.vectorincng.squareandroidtaskvictor.network.EmployeeFetcher
 import com.vectorincng.squareandroidtaskvictor.runBlockingTest
+import com.vectorincng.squareandroidtaskvictor.utilities.testEmployees
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import io.mockk.coEvery
+import io.mockk.mockk
+import junit.framework.TestCase
+import kotlinx.coroutines.flow.flow
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.RuleChain
-import javax.inject.Inject
 import kotlin.jvm.Throws
 
 @HiltAndroidTest
@@ -27,13 +33,11 @@ class HomeViewModelTest {
         .around(instantTaskExecutorRule)
         .around(coroutineRule)
 
-    @Inject
-    lateinit var employeesRepository: EmployeesRepository
+    private var employeesRepository: EmployeesRepository = mockk()
 
     @Before
     fun setUp() {
         hiltRule.inject()
-
         viewModel = HomeViewModel(employeesRepository)
     }
 
@@ -42,5 +46,34 @@ class HomeViewModelTest {
     @Throws(InterruptedException::class)
     fun testDefaultValues() = coroutineRule.runBlockingTest {
         Assert.assertTrue(viewModel.state.value == HomeScreenUiState.Loading)
+
+    }
+
+    @Suppress("BlockingMethodInNonBlockingContext")
+    @Throws(InterruptedException::class)
+    @Test
+    fun testFetchResultSuccessState() = coroutineRule.runBlockingTest {
+        val result = flow { emit(EmployeeFetcher.EmployeeDataResponse.Success(testEmployees)) }
+        coEvery { employeesRepository.updateEmployeeList(true) } returns result
+        viewModel.refresh(true)
+
+        viewModel.state.test {
+            TestCase.assertEquals(HomeScreenUiState.Loading, awaitItem())
+            TestCase.assertEquals(HomeScreenUiState.Ready(testEmployees), awaitItem())
+        }
+    }
+
+    @Suppress("BlockingMethodInNonBlockingContext")
+    @Throws(InterruptedException::class)
+    @Test
+    fun testFetchResultErrorState() = coroutineRule.runBlockingTest {
+        val result = flow { emit(EmployeeFetcher.EmployeeDataResponse.Error(throwable = null)) }
+        coEvery { employeesRepository.updateEmployeeList(true) } returns result
+        viewModel.refresh(true)
+
+        viewModel.state.test {
+            TestCase.assertEquals(HomeScreenUiState.Loading, awaitItem())
+            TestCase.assertEquals(HomeScreenUiState.Error(null), awaitItem())
+        }
     }
 }
